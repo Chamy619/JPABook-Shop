0caba930c63f6a27c59959780f9be1926c22c2e2
Chamy619 <yd95069506@naver.com>
2025-11-13 19:41:49 +0900

    feat: 주문 컨트롤러 추가

---
+++ src/main/java/jpashop/web/OrderController.java
@@ -0,0 +1,43 @@
+package jpashop.web;
+
+import jpashop.domain.Member;
+import jpashop.domain.item.Item;
+import jpashop.service.ItemService;
+import jpashop.service.MemberService;
+import jpashop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    ItemService itemService;
+
+    @RequestMapping(value = "/order", method = RequestMethod.GET)
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @RequestMapping(value = "/order", method = RequestMethod.POST)
+    public String order(@RequestParam("memberId") Long memberId, @RequestParam("itemId") Long itemId, @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count);
+        return "redirect:orders";
+    }
+}
